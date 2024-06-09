30bf2ac2fb7e46c667fb154a764a8b380aff021b
Dalton <cdaltonrowe@gmail.com>
2024-06-09 13:29:14 +0200

    l8 spec

---
+++ playwright/tests/level8.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has correct text after select options set', async ({ page }) => {
+  await page.goto('level8');
+  const selects = await page.locator("select");
+  const selectOne = selects.nth(0)
+  const selectTwo = selects.nth(1)
+  const selectThree = selects.nth(2)
+  const pre = await page.locator('pre')
+
+  await selectOne.selectOption('hamburger')
+  await expect(pre).toHaveText('Yummy')
+
+  await selectTwo.selectOption('cheese')
+  await expect(pre).toHaveText('Even better')
+
+  await selectThree.selectOption(['lettuce', 'tomato', 'onion'])
+  await expect(pre).toHaveText('Just how I like it!')
+});
