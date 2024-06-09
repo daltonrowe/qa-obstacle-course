d9654b39305ae5afcfa8873455f1425821118b05
Dalton <cdaltonrowe@gmail.com>
2024-06-09 13:22:34 +0200

    l7 test

---
+++ playwright/tests/level7.spec.js
@@ -0,0 +1,24 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has input that responds to focus and blur', async ({ page }) => {
+  await page.goto('level7');
+  const input = await page.locator("input")
+  const pre = await page.locator("pre")
+
+  await input.focus();
+  await expect(pre).toHaveText('Focusing')
+
+  await input.blur();
+  await expect(pre).toHaveText('Blurred')
+});
+
+test('has input that triggers text when value is correct', async ({ page }) => {
+  await page.goto('level7');
+  const input = await page.locator("input")
+  const pre = await page.locator("pre")
+
+
+  await input.fill('4321');
+  await expect(pre).toHaveText('Woo hoo!')
+});
