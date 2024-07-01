4f5e64bdf58aa36751ddcd6832a6606ceae1efbf
Dalton <cdaltonrowe@gmail.com>
2024-07-01 11:05:27 +0200

    enfore consistency

--- playwright/tests/_meta.spec.js
+++ playwright/tests/_meta.spec.js
@@ -1,7 +1,32 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
+import * as fs from "fs"
 
-test('has title', async ({ page }) => {
-  await page.goto('level1');
-  await expect(page).toHaveTitle("Level One");
-});
+const index = fs.readFileSync('./public/index.html', { encoding: 'utf8' })
+const regex = /href="level([0-9]+)"/g;
+const hrefs = index.match(regex) ?? []
+
+hrefs.forEach((_href, index) => {
+  // prevent repo structure runs on live site
+  if (!(process.env.CI || process.env.LOCAL)) return;
+
+  const level = index + 1
+
+  test(`has proper structure for level ${level}`, async ({ page }) => {
+    page.goto(`level${level}`)
+    await expect(page).toHaveTitle(`Level ${level}`)
+
+    const desc = page.locator('meta[name="description"]');
+    await expect(desc).toHaveCount(1)
+
+    const next = page.locator(`a[href="level${level + 1}"]`);
+    await expect(next).toHaveCount(1)
+
+    const icon = page.locator('link[rel="shortcut icon"]')
+    await expect(icon).toHaveCount(1)
+    await expect(icon).toHaveAttribute('href', 'favicon.svg')
+
+    const goals = page.locator(".box.goals");
+    await expect(goals).toBeVisible();
+  });
+})
