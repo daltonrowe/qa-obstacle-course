5de77e4b43fa0c70fe9a031c5f407186c82773fe
Dalton <cdaltonrowe@gmail.com>
2025-02-24 08:39:04 +0100

    biome lint

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ const port = portFlagIndex !== -1 ? process.argv[portFlagIndex + 1] : 3000;
 
 app.use(express.static("public", { extensions: ["html"] }));
 
-fs.readdirSync("api").forEach(async (file) => {
+const files = fs.readdirSync("api");
+for (const file of files) {
   const { default: route } = await import(`./api/${file}`);
   const level = file.split(".")[0];
   app.use(`/api/${level}`, route);
-});
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
